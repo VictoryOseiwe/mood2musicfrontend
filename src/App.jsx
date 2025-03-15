90d1ee1fd8fc49abb554b9dcf216ba6b2917749c
Victory Oseiwe <thisisvictoryoseiwe@gmail.com>
2025-03-15 06:11:10 +0100

    working on client home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-
+import Home from "./pages/home/Home";
 export default function App() {
   return (
     <>
       <div>
-        <h1>Mood 2 Music</h1>
-        <p>Get it lit</p>
-        <a href="djdf">something</a>
-        <ul>
-          <li>2</li>
-          <li>4</li>
-          <li>6</li>
-        </ul>
+        <Home />
       </div>
     </>
   );
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to HabitTracker</h1>
+      <p>Track your daily habits and see progress over time.</p>
+    </div>
+  );
+}
